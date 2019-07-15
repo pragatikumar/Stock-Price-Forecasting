2226da6e40fd64da5da5e037df71cf0dcd89c8e2
PRAGATI KUMAR <34084017+pragatikumar@users.noreply.github.com>
2019-07-15 17:53:17 +0530

    Add files via upload

---
+++ make_visualization.py
@@ -0,0 +1,148 @@
+import sys
+import json
+import warnings
+
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+
+from train_agent import training_parser
+from Make_Prediction.ARIMA import ARIMA_implementation
+
+class visualization():
+    '''
+    The goal of this class is to visualize the data
+    '''
+    def __init__(self, stock_choice, type):
+        warnings.filterwarnings("ignore")
+
+        self.stock_choice = stock_choice
+        self.type = type
+
+        self.main()
+
+    def get_stock_values(self):
+        '''
+        The goal of this function is to return optimal values
+        '''
+        #Gets the dictionary of values that were saved to the JSON file
+        with open('E:\ML\project\Data/best_parameters.json') as file_save:
+            try:
+                current_dictionary = json.load(file_save)
+            except json.decoderself.JSONDecodeError:
+                current_dictionary = {}
+
+        trained = False
+        #Loop through the dictionary to see if the stock has saved values
+        for i in current_dictionary:
+            #The stock has optimal values
+            if str(i) == str(self.stock_choice):
+                trained = True
+
+                optimal_values_string = current_dictionary[i]
+                p = int(optimal_values_string[1])
+                d = int(optimal_values_string[4])
+                q = int(optimal_values_string[7])
+
+                optimal_values = (p,d,q)
+                break
+
+        #The stock does NOT have optimal values
+        if trained == False:
+            # Ask the user if they would like to train the stock
+            # This is because if the stock has not optimal values, then
+            # It has not been trainined for, and could be done now
+
+            print("")
+            print("This stock has not been trained for. Do you want to do that now? [y/n]")
+            answer = input()
+
+            if answer.lower() == "y" or answer.lower() == "yes":
+                train_stock = training_parser()
+                train_stock.start_training(self.stock_choice)
+
+            elif answer.lower() == "n" or answer.lower() == "no":
+                sys.exit(0)
+
+            else:
+                print("")
+                print("Enter a valid response.")
+                #Repeats the loop if asking the user
+
+                self.get_stock_values()
+
+        else:
+            #Continue through the Hierarchy of visualizing the data
+            data = ARIMA_implementation(self.stock_choice)
+            stationary_data, data = data.get_data()
+            return optimal_values, stationary_data, data
+
+
+
+    def predict_future_values(self, optimal_values, stationary_data, data):
+        '''
+        The goal of this function is to get and return predicted values
+        '''
+        self.data = data
+        if self.type == "simple":
+            self.show_graph_simple()
+            sys.exit(0)
+
+        self.data_length = len(data)
+        #print(stationary_data)
+        self.number_of_predictions = len(data)-int(int(len(data))*.9)
+
+        updated_data = [x for x in data]
+        updated_data=updated_data[:int(int(len(data))*.9)]
+        predictions = []
+
+        for i in range(self.number_of_predictions):
+            model = sm.tsa.statespace.SARIMAX(updated_data, order = optimal_values, enforce_invertibility=False)
+            model_fit = model.fit(disp=0)
+            forecast = model_fit.forecast(1)[0]
+            updated_data.append(data[int(int(len(data))*.9)+i])
+
+            predictions.append(forecast)
+
+        return predictions, updated_data
+
+    def show_graph_forecast(self, predicted_values, data_with_predictions):
+        '''
+        The goal of this function is to show to graph, with the forecasted values
+        '''
+        predicted_data = []
+        full_data = [x for x in self.data]
+        full_data=full_data[:int(len(full_data)*.9)]
+        index = len(full_data) - 1
+        for i in range(self.number_of_predictions):
+            value = (predicted_values[i])
+            full_data.append(value)
+            predicted_data.append(value)
+
+        plt.plot(full_data, color="red", label = "Forecasted Data")
+        plt.plot(self.data, color="blue", label = "Previous Data")
+        plt.legend(loc = "upper left")
+        plt.show()
+        error = mean_squared_error(self.data,full_data)
+        print("Mse error=",error)
+
+    def show_graph_simple(self):
+        '''
+        The goal of this function is the show to graph, with only the normal data
+        '''
+        # Add the simple data
+        plt.plot(self.data)
+
+        plt.xlabel('Progress of Time')
+        plt.ylabel('Price (USD)')
+        plt.title("%s Stock Price" %(self.stock_choice))
+
+        plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+
+        plt.show()
+
+    def main(self):
+        optimal_values, stationary_data, data = self.get_stock_values()
+        predicted_values, data_with_predictions = self.predict_future_values(optimal_values, stationary_data, data)
+
+        self.show_graph_forecast(predicted_values, data_with_predictions)
---
+++ train_agent.py
@@ -0,0 +1,131 @@
+import sys
+import json
+import argparse
+
+from Data.stock_choices import list_of_stocks
+from Make_Prediction.ARIMA import ARIMA_implementation
+
+class training_parser():
+    '''
+    The job of this class is to take the user's arguments as input and decide what
+    should happen next
+    '''
+
+    def __init__(self):
+        #Gets the list of stock names
+        self.list_of_stocks = list_of_stocks
+
+    def create_parser(self):
+        '''
+        The goal of this function is to create the parser
+        '''
+        self.training_parser = argparse.ArgumentParser()
+        self.add_arguments()
+
+    def add_arguments(self):
+        '''
+        The goal of this function is to add all necessary arguments to the parser
+        '''
+        #Allow user to choose what stock to train for
+        self.training_parser.add_argument('--choice')
+
+        #Allow the user to see a list of all available
+        self.training_parser.add_argument('--list')
+
+        #Allow the user to see what stocks already have optimal parameters trained for
+        self.training_parser.add_argument('--trained')
+
+        self.args = self.training_parser.parse_args()
+        self.check_arguments()
+
+    def check_arguments(self):
+        '''
+        The goal of this function is to take the users input and decide what the
+        output should be. This is where the logic is held
+        '''
+        #Check to see if the stock was an acceptable input
+        length = len(list_of_stocks)
+        for i in list_of_stocks:
+            if self.args.choice == i:
+                self.stock_choice = i
+                #If the stock was acceptable, break the loop
+                break
+            length -= 1
+
+        if length == 0 and self.args.choice is not None:
+            #This only happens when the stock was not a valid name
+            print("")
+            print("Enter a valid stock!")
+            sys.exit(0)
+
+        #This will show the list of stocks that are acceptable to train for
+        if self.args.list == "show":
+            print("")
+            print("The list of stocks are: ")
+            print("")
+            print(list_of_stocks)
+            sys.exit(0)
+
+        #This wlil show the user the stocks that already have optimal variabels
+        if self.args.trained == "show":
+            with open('E:\ML\project\Data/best_parameters.json') as file_save:
+                try:
+                    current_dictionary = json.load(file_save)
+                except json.decoderself.JSONDecodeError:
+                    current_dictionary = {}
+            print("")
+            print("Stocks that are already trainined for are:")
+            print("")
+            print('%s'%(current_dictionary))
+            sys.exit()
+
+        try:
+            #Start the training
+            self.start_training(self.stock_choice)
+        except AttributeError:
+            #This only happens when the user did not provide any arguments
+            print("")
+            print("""Use a command:
+                    [--list show] prints off the list of available stocks
+                    [--trained show] prints the stocks that already have been trainined for
+                    [--choice *STOCK NAME*] allows you to train the model for a particular stock""")
+            sys.exit(0)
+
+    def start_training(self, stock_choice):
+        '''
+        The goal of this function is to start the training
+        '''
+        # Self.stock_choice is repeated because the visualize operation can call to train the stock, this allows the
+        # Training process to know what stock to train for without adding additional functions
+        self.stock_choice = stock_choice
+        ARIMA = ARIMA_implementation(self.stock_choice)
+        self.best_order = ARIMA.main()
+
+        self.save_order()
+
+    def save_order(self):
+        '''
+        The goal of this function is to save the optimal (p,d,q) values for future use
+        '''
+        #Saves the order to a JSON file
+        with open('E:\ML\project\Data/best_parameters.json') as file_save:
+            try:
+                current_dictionary = json.load(file_save)
+            except json.decoderself.JSONDecodeError:
+                #This gets called if the JSON file is empty
+                current_dictionary = {}
+
+        with open('E:\ML\project\Data/best_parameters.json', 'w') as file_save:
+            #Saves the values
+            current_dictionary[str(self.stock_choice)] = str(self.best_order)
+            json.dump(current_dictionary, file_save)
+
+    def main(self):
+        '''
+        The goal of this function is to start the whole process
+        '''
+        self.create_parser()
+
+if __name__ == "__main__":
+    train = training_parser()
+    train.main()
